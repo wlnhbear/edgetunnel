05fadc4390c90ab3f5a6ee9406ba6809de7f9604
wlnhbear <57051665+wlnhbear@users.noreply.github.com>
2024-12-18 15:54:20 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '15930900-8594-431b-94f7-7e0417f8e43a';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
